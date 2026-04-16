3f35adb98b19bfb8e2e96adebbfdeac8d511dc6c
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-04-16 11:34:07 +0300

    first commit

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const container = document.getElementById("anime-container");
 const searchInput = document.getElementById("search");
+
+async function loadAnime(query = "naruto") {
+  container.innerHTML = "Loading...";
+  const res = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`);
+  const data = await res.json();
+}
